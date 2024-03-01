9425b1645ba89415923a50a32c374cf3f36b8b08
Pavel Eroshin <stpashik@gmail.com>
2024-03-01 10:11:41 +0300

    update for version 1.22.13 (3124);

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "InAppStorySDK",
-                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.22.12/InAppStorySDK-1.22.12.xcframework.zip",
-                   checksum: "a47d43c966b4a35bf74f3e835277fb880d687384dc3a95600b44e11155a8c982"
+                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.22.13/InAppStorySDK-1.22.13.xcframework.zip",
+                   checksum: "71afaf2c814464862b5f14c8a12a04f3bb92719a18e838694de949e4bff9ea0a"
                )
     ],
     swiftLanguageVersions: [.v5]
