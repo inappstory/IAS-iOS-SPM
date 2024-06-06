23d372ce77540275c5188fb33971545ca1481fda
Pavel Eroshin <stpashik@gmail.com>
2024-06-06 16:35:24 +0300

    update for version 1.23.5 (3224);

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "InAppStorySDK_SwiftUI",
-                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.23.4-SwiftUI/InAppStorySDK_SwiftUI-1.23.4.xcframework.zip",
-                   checksum: "f2e7dc87ee66db0bbca89978e08c0064a953460dc30071e6fb2421b422efb833"
+                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.23.5-SwiftUI/InAppStorySDK_SwiftUI-1.23.5.xcframework.zip",
+                   checksum: "11992dab79ec2ffa369803d4bbe3a6f8d11195387e73dd7e95471c1a367507b0"
                )
     ],
     swiftLanguageVersions: [.v5]
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Repository for reducing version resolve time in Swift Package Manager
 
 | InAppStory version | Build version | iOS SwiftUI version |
 |--------------------|---------------|---------------------|
-| 1.23.4             | 3220          | >= 13.0             |
+| 1.23.5             | 3224          | >= 13.0             |
 
 Version of the library can be obtained from the parameter `InAppStory.buildInfo`
 
