1063fe85cab51dc056026c475246de705f8ed500
StPashik <stpashik@gmail.com>
2024-12-17 08:48:39 +0300

    update for version 1.24.10 (3458);

--- Package.swift
+++ Package.swift
@@ -14,10 +14,9 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "InAppStorySDK_SwiftUI",
-                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.24.9-SwiftUI/InAppStorySDK_SwiftUI-1.24.9.xcframework.zip",
-                   checksum: "f177b40abd153b8564fbe87daa854514aff0f53829a32f26c60ed74ae2f04569"
+                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.24.10-SwiftUI/InAppStorySDK_SwiftUI-1.24.10.xcframework.zip",
+                   checksum: "f0a8ed1a3da22666424d8e8674766a17ec735e9d6f62d026843fa6769fc7469c"
                )
     ],
     swiftLanguageVersions: [.v5]
 )
-
