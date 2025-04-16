88332bc1bf5024015d63c700a6a4889713ecffbd
StPashik <stpashik@gmail.com>
2025-04-16 17:04:39 +0300

    update for version 1.25.1 (3602);

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "InAppStorySDK_SwiftUI",
-                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.25.0-SwiftUI/InAppStorySDK_SwiftUI-1.25.0.xcframework.zip",
-                   checksum: "c1f937f0056e7219b6d0d28c89be129693db21857428faedf8258bde847891f6"
+                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.25.1-SwiftUI/InAppStorySDK_SwiftUI-1.25.1.xcframework.zip",
+                   checksum: "e7ccaad6e9d24b69e50be9022b5ca05a76234ad3759895daf655d19fd04dd408"
                )
     ],
     swiftLanguageVersions: [.v5]
