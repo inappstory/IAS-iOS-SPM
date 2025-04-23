e461eff5a2e13371b1fdfdf9d1e6c6ba20c5ab0e
StPashik <stpashik@gmail.com>
2025-04-23 17:09:14 +0300

    update for version 1.25.2 (3604);

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "InAppStorySDK",
-                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.25.1/InAppStorySDK-1.25.1.xcframework.zip",
-                   checksum: "4360f771c3658348bee565cf44e3c4c5da78917827a207c92d22635b56618a95"
+                   url: "https://github.com/inappstory/ios-sdk/releases/download/1.25.2/InAppStorySDK-1.25.2.xcframework.zip",
+                   checksum: "08f6f129d291aa842ddbf32582efced7977cb1592c26674e5a5514ade21365be"
                )
     ],
     swiftLanguageVersions: [.v5]
