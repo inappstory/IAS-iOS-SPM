ec615ca8b45c3948e8cfe0d01207376d5e2fe170
StPashik <stpashik@gmail.com>
2025-10-15 18:23:58 +0300

    update for version 1.26.0-RC8 (3845)

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "InAppStorySDK",
-                   url: "https://github.com/inappstory/ios-ias-sdk/releases/download/1.26.0-RC7/InAppStorySDK-1.26.0-RC7.xcframework.zip",
-                   checksum: "da56bc0755f81e6d77fd1b7286a09aa9356781d951a83331eeee6a315a76f944"
+                   url: "https://github.com/inappstory/ios-ias-sdk/releases/download/1.26.0-RC8/InAppStorySDK-1.26.0-RC8.xcframework.zip",
+                   checksum: "9c7abe619124c7607247e97cbbb93f2e025b18a46b09d48c6479b056dc8e0894"
                )
     ],
     swiftLanguageVersions: [.v5]
