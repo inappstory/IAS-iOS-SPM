b616288b362d74d49ef994a3b540f37527a4dec0
InAppStory CI <ci@inappstory.com>
2026-08-05 23:25:09 -0700

    Update for version 1.29.4 (4324);

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "InAppStorySDK_SwiftUI",
-                   url: "https://github.com/inappstory/ios-ias-sdk/releases/download/1.29.3-SwiftUI/InAppStorySDK_SwiftUI-1.29.3.xcframework.zip",
-                   checksum: "320b6f9fa82705c8e4e09a6d1b4461c07c965aca26d790a95ededa96c6a731ca"
+                   url: "https://github.com/inappstory/ios-ias-sdk/releases/download/1.29.4-SwiftUI/InAppStorySDK_SwiftUI-1.29.4.xcframework.zip",
+                   checksum: "6dbd41a414ddbff2bbdbd2122836050ad87603506fda8b4497a945132219e653"
                )
     ],
     swiftLanguageVersions: [.v5]
