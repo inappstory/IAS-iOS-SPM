bc44d469f58f25263cc6caf105d4d35434a1dcc1
InAppStory CI <ci@inappstory.com>
2026-08-28 05:01:00 -0700

    Update for version 1.29.6 (4334);

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "InAppStorySDK",
-                   url: "https://github.com/inappstory/ios-ias-sdk/releases/download/1.29.5/InAppStorySDK-1.29.5.xcframework.zip",
-                   checksum: "987d3c52b1306b0777d97013c75368ec2dbe2ecb3f2ae54ac69a4fdc3221dfda"
+                   url: "https://github.com/inappstory/ios-ias-sdk/releases/download/1.29.6/InAppStorySDK-1.29.6.xcframework.zip",
+                   checksum: "0d926147bb06e7b99cb5292e6b96615e018547c9ec9f465974d76afe683178d6"
                )
     ],
     swiftLanguageVersions: [.v5]
